fac2fb324e160f3349f3089002f76baf0c268ca9
Lenkaa372 <allanlenkaa@gmail.com>
2022-09-25 11:50:49 +0300

    adding the offroad  Route

---
+++ app/controllers/offroad_controller.rb
@@ -0,0 +1,5 @@
+class OffroadController <ApplicationController
+    def index
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,3 +5,10 @@ Rails.application.routes.draw do
   # root "articles#index"
   get "/racing", to: "racing#index"
 end
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get "/offroad", to: "offroad#index"
+end
